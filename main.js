3b2899ce09cca0bc1d030358d48099eabc1848a3
jnel24 <josh.nelson2424@gmail.com>
2020-02-21 12:49:01 -0600

    Updated main.js and .params

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
-/**
- * Copyright 2017 IBM Corp. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the 'License'); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
+// Authored By:
+// Cameron Mathis, Natalie Eichorn, Michelle Williams, Josh Nelson
+// February 2020
 
 'use strict';
 
-const alexaVerifier = require('alexa-verifier');
-const AssistantV1 = require('ibm-watson/assistant/v1');
+const AssistantV2 = require('ibm-watson/assistant/v2');
 const { IamAuthenticator } = require('ibm-watson/auth');
+const alexaVerifier = require('alexa-verifier');
+
+// Using some globals for now
+let assistant;
+let context;
 
 function errorResponse(reason) {
   return {
@@ -33,9 +25,6 @@ function errorResponse(reason) {
   };
 }
 
-// Using some globals for now
-let assistant;
-let context;
 
 function verifyFromAlexa(args, rawBody) {
   return new Promise(function(resolve, reject) {
@@ -51,98 +40,151 @@ function verifyFromAlexa(args, rawBody) {
   });
 }
 
-function initClients(args) {
-  // Connect a client to Watson Assistant
-  assistant = new AssistantV1({
-    version: '2020-02-02',
-    authenticator: new IamAuthenticator({ apikey: args.ASSISTANT_APIKEY }),
-    serviceUrl: args.ASSISTANT_URL
-  });
 
-  console.log('Connected to Watson Assistant');
+function createAssistant()
+{
+	return new Promise(function(resolve, reject)
+	{
+		console.log("Instantiate the AssistantV2")
+		assistant = new AssistantV2(
+			{
+  				version: '2020-02-05',
+  				authenticator: new IamAuthenticator(
+				{
+    				apikey: 'HUFciyT6Ch2r5myueZYgZPG6Fabjw4Kd1CiJTPiLvm0Y',
+  				}),
+  				url: 'https://api.us-south.assistant.watson.cloud.ibm.com/instances/5f3ef3b3-0293-47e9-9806-97f8d3e35750',
+			});
+		resolve()
+	});
 }
 
-function assistantMessage(request, skillId) {
-  return new Promise(function(resolve, reject) {
-    const input = request.intent ? request.intent.slots.EveryThingSlot.value : 'start skill';
-    console.log('SKILL_ID: ' + skillId);
-    console.log('Input text: ' + input);
+function createSessionID()
+{
+	return new Promise(function(resolve,reject)
+	{
+		console.log("Create Session")
+		assistant.createSession(
+		{
+			assistantId: '66f26318-759d-4eae-82bf-0e64534dcc4e'
+		})
+		.then(res => 
+		{
+			session_identifier = JSON.stringify(res.result.session_id, null, 2);
+			session_json = res;	
+			console.log(session_identifier);
+			console.log(JSON.stringify(session_json, null, 2));
+			resolve(session_json);
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	});
+}
 
-    assistant.message(
-      {
-        input: { text: input },
-        workspaceId: skillId,
-        context: context
-      },
-      function(err, watsonResponse) {
-        if (err) {
-          console.error(err);
-          reject(Error('Error talking to Watson.'));
-        } else {
-          console.log('Watson result: ', watsonResponse.result);
-          context = watsonResponse.result.context; // Update global context
-          resolve(watsonResponse);
-        }
-      }
-    );
-  });
+function communicateWithWatson(request, session_json)
+{
+	return new Promise(function(resolve,reject)
+	{
+		console.log("Communicate with watson")
+		const input = request.intent ? request.intent.slots.everythingslot.value : 'welcome';
+    	console.log('Input text: ' + input);
+		assistant.message(
+		{
+  			assistantId: '66f26318-759d-4eae-82bf-0e64534dcc4e',
+  			sessionId: session_json.result.session_id,
+  			input: 
+			{
+    			text: input
+    		}
+  		})
+  		.then(res => 
+			{
+    			console.log(JSON.stringify(res, null, 2));
+				resolve(session_json);
+  			})
+  			.catch(err => 
+			{
+    			console.log(err);
+  			});
+	});
+}
+
+function terminateSession()
+{
+	console.log("Terminate Session")
+	assistant.deleteSession(
+	{
+  		assistantId: '66f26318-759d-4eae-82bf-0e64534dcc4e',
+  		sessionId: session_json.result.session_id,
+	})
+  	.then(res => 
+	{
+    	console.log(JSON.stringify(res, null, 2));
+  	})
+  	.catch(err => 
+	{
+    	console.log(err);
+  	});
+	return;
 }
 
 function sendResponse(response, resolve) {
-  console.log('Begin sendResponse');
-  console.log(response);
+	console.log('Begin sendResponse');
+	console.log(response);
+  
+	// Combine the output messages into one message.
+	const output = response.result.output.text.join(' ');
+	console.log('Output text: ' + output);
 
-  // Combine the output messages into one message.
-  const output = response.result.output.text.join(' ');
-  console.log('Output text: ' + output);
-
-  // Resolve the main promise now that we have our response
-  resolve({
-    version: '1.0',
-    response: {
-      shouldEndSession: false,
-      outputSpeech: {
-        type: 'PlainText',
-        text: output
-      }
-    },
-    sessionAttributes: { watsonContext: context }
-  });
-}
+	// Resolve the main promise now that we have our response
+	resolve({
+	  version: '1.0',
+	  response: {
+		shouldEndSession: false,
+		outputSpeech: {
+		  type: 'PlainText',
+		  text: output
+		}
+	  },
+	  sessionAttributes: { watsonContext: context }
+	});
+  }
 
 function main(args) {
-  console.log('Begin action');
-  // console.log(args);
-  return new Promise(function(resolve, reject) {
-    if (!args.__ow_body) {
-      return reject(errorResponse('Must be called from Alexa.'));
-    }
-
-    const rawBody = Buffer.from(args.__ow_body, 'base64').toString('ascii');
-    const body = JSON.parse(rawBody);
-
-    // Alexa attributes hold our context
-    const alexaAttributes = body.session.attributes;
-    console.log('Alexa attributes:');
-    console.log(alexaAttributes);
-    if (typeof alexaAttributes !== 'undefined' && Object.prototype.hasOwnProperty.call(alexaAttributes, 'watsonContext')) {
-      context = alexaAttributes.watsonContext;
-    } else {
-      context = {};
-    }
-
-    const request = body.request;
-
-    verifyFromAlexa(args, rawBody)
-      .then(() => initClients(args))
-      .then(() => assistantMessage(request, args.SKILL_ID))
-      .then(watsonResponse => sendResponse(watsonResponse, resolve))
-      .catch(err => {
-        console.error('Caught error: ');
-        console.log(err);
-        reject(errorResponse(err));
-      });
-  });
+	console.log('Begin action');
+	return new Promise(function(resolve, reject)
+	{
+		if (!args.__ow_body) {
+			return reject(errorResponse('Must be called from Alexa.'));
+		  }
+	  
+		  const rawBody = Buffer.from(args.__ow_body, 'base64').toString('ascii');
+		  const body = JSON.parse(rawBody);
+	  
+		  // Alexa attributes hold our context
+		  const alexaAttributes = body.session.attributes;
+		  console.log('Alexa attributes:');
+		  console.log(alexaAttributes);
+		  if (typeof alexaAttributes !== 'undefined' && Object.prototype.hasOwnProperty.call(alexaAttributes, 'watsonContext')) {
+			context = alexaAttributes.watsonContext;
+		  } else {
+			context = {};
+		  }
+	  
+		const request = body.request;
+		verifyFromAlexa(args, rawBody)
+			.then(() => createAssistant())
+			.then(() => createSessionID()) 
+			.then(session_json => communicateWithWatson(request, session_json))
+			.then(session_json => sendResponse(session_json, resolve))
+			.catch(err =>
+			{
+				console.log(err);
+				reject(errorResponse(err));
+			});
+	}); 
 }
 
-exports.main = main;
+exports.main = main
+require('make-runnable');
